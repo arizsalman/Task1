fc2675a6c8662b823ae363e389d4ab0a7e4e8b23
arizsalman <muhammadarizsalman@gmail.com>
2025-07-08 11:06:33 +0500

    SASS  Css Complete mini porject

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+
+
+<header className="site-header">
+  <nav className="navbar">
+    <div className="logo">MySite</div>
+    <ul className="nav-links">
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Menu</a>
+      </li>
+      <li>
+        <a href="#">Contact Us</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+     <div className="App">
+      <h1>Add to SASS CSS</h1>
+    </div>
+
+
+    <div className="container">
+      <div className="box sass-box">Sass</div>
+      <div className="box css-box">CSS</div>
+    </div>
+
+    <footer className="site-footer">
+    <div className="footer-content">
+      <h2 className="footer-logo">MyFoodSite</h2>
+      <p className="footer-description">
+        Serving delicious meals and unforgettable experiences since 2020. We
+        value taste, freshness, and your satisfaction.
       </p>
+      <p className="copyright">© 2025 MyFoodSite. All rights reserved.</p>
+    </div>
+  </footer>
     </>
   )
 }
